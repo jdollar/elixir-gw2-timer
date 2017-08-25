c9e9944026b13d63e9bc586ae942e9f931bcd011
jdollar <dollarhidejoel@gmail.com>
2017-08-24 22:04:28 -0400

    updated the world boss page to try and flesh out a layout
    messed around with bulma. Tried out some grid styling
    tried adding timex for elixir and started parsing out the time object

--- lib/gw2timer_web/controllers/worldboss_controller.ex
+++ lib/gw2timer_web/controllers/worldboss_controller.ex
@@ -1,11 +1,17 @@
 defmodule Gw2timerWeb.WorldBossController do
   use Gw2timerWeb, :controller
+  import Ecto.Query
   alias Gw2timer.WorldBoss
 
   def index(conn, _params) do
-    world_bosses = Gw2timer.Repo.all(WorldBoss)
-                    |> Gw2timer.Repo.preload(:zone)
-                    |> Gw2timer.Repo.preload(:event_times)
+    world_bosses = Gw2timer.Repo.all from world_bosses in WorldBoss,
+                                     left_join: zones in assoc(world_bosses, :zone),
+                                     left_join: event_times in assoc(world_bosses, :event_times),
+                                     group_by: world_bosses.id,
+                                     preload: [:zone, :event_times]
+
+    IO.inspect world_bosses
+
     render conn, "index.html", world_bosses: world_bosses
   end
 end
--- lib/gw2timer_web/views/worldboss_view.ex
+++ lib/gw2timer_web/views/worldboss_view.ex
@@ -1,3 +1,9 @@
 defmodule Gw2timerWeb.WorldBossView do
   use Gw2timerWeb, :view
+  use Timex
+  import Ecto.Time
+
+  def format_event_time(event_time) do
+      "#{event_time.hour}:#{event_time.minute}"
+  end
 end
